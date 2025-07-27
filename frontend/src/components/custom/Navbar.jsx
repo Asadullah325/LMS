17cd4a6691dc355bdedb334f6fa7211fc83c72ac
Asadullah325 <asadullah456545@gmail.com>
2025-07-27 20:07:08 +0500

    Tailwind setup and mongodb connected

---
+++ frontend/src/components/custom/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Navbar
